65b5e77ac43127386fa7ebe13d23a9c64a56743a
Rui.Zhou <156621293@qq.com>
2017-04-09 21:25:19 +0800

    新增布局

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <el-button @click.native="startHacking">Let's do it</el-button>
+    <div class="top hbox">
+      <div class="logo">海里巴巴</div>
+      <div class="center">
+        <el-input
+          placeholder="搜索功能"
+          icon="search"
+          v-model="searchMenu">
+        </el-input>
+      </div>
+      <div class="menu">
+        <i class="el-icon-more"></i>
+        <i class="el-icon-information"></i>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,6 +21,7 @@
 export default {
   data () {
     return {
+      searchMenu:'',
       msg: 'Use Vue 2.0 Today!'
     }
   },
@@ -27,7 +39,62 @@ export default {
 </script>
 
 <style>
-body {
+.hbox{
+  display:-moz-box;
+  display:-webkit-box;
+  display:flex;
+  -webkit-box-orient:horizontal;
+  -moz-box-orient:horizontal;
+  box-orient:horizontal;
+  -webkit-flex-flow:row;
+  flex-flow:row;
+}
+.vbox{
+  display:-moz-box;
+  display:-webkit-box;
+  display:flex;
+  box-orient:vertical;
+  -webkit-box-orient:vertical;
+  -moz-box-orient:vertical;
+  -webkit-flex-flow:column;
+  flex-flow:column;
+}
+.flex1{
+  -webkit-box-flex:1;
+  -webkit-flex:1;
+  -moz-flex:1;
+  -moz-box-flex:1;
+  flex:1;
+  width:0;
+}
+html,body {
   font-family: Helvetica, sans-serif;
+  margin:0;
+  padding:0;
+}
+.top{
+  height:47px;
+  line-height:47px;
+  border-bottom:1px solid #e5e5e5;
+  padding:0 30px;
+}
+.top .center{
+  margin:0 auto;
+  width:200px;
+}
+.top .logo{
+  color:#333;
+  font-size:20px;
+  font-weight:500;
+  display:inline-block;
+}
+.menu{
+  line-height:47px;
+}
+.menu > i{
+  line-height:normal;
+  font-size:20px;
+  color:#00aaff;
+  margin-left:20px;
 }
 </style>
